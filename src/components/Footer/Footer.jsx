76b103bb75891aee44a9cd815d5b11358b1e35d8
Lucas Arguello <lucas5ivan@hotmail.com>
2023-05-19 12:58:08 -0300

    modificaciones en el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
-
-export const Footer= () => {
-    return (
-    
-            <div className="text-center bg-info py-4 mt-5">
-              <p className="d-flex-center ">
-                @Copyright 2023 Mi Vivero Virtual | Creada por: 
-                <Link className="autor" to="/">  Lucas Ivan Arguello  </Link>
-              </p>
-            </div>
-
-              
-      
-    )
-  }
-  
-  
+export const Footer = () => {
+  return (
+    <div className="bg-info py-4 mt-5 fixed-bottom">
+      <Container className="text-center">
+        <p className="d-flex-center">
+          © 2023 Mi Vivero Virtual | Creada por:
+          <Link className="autor" to="/">
+            Lucas Ivan Arguello
+          </Link>
+        </p>
+      </Container>
+    </div>
+  );
+};
